43a905cbd12a863c235b95cd50156e001cd3aa75
Dav Clark <ClarkDa@kennedykrieger.org>
2018-01-29 19:35:19 -0500

    Working towards working audio

--- scripts/ffmpeg-fujisawa.sh
+++ scripts/ffmpeg-fujisawa.sh
@@ -1,7 +1,11 @@
-# This seems to use vaapi for decoding giving us just a little edge that we
-# need to nail full frame rate
-
-# -hwaccel vaapi -vaapi_device /dev/dri/renderD128 \
-
-ffmpeg \
-  -f v4l2 -framerate 30 -video_size hd720 -pixel_format yuv420p -i /dev/video0 output.avi
+ffmpeg -hwaccel cuvid \
+  -f v4l2 -framerate 30 -video_size hd720 -pixel_format yuv420p \
+  -thread_queue_size 1024 \
+  -i /dev/video1 \
+  -f alsa -thread_queue_size 1024 \
+  -i hw:4 \
+  -threads 4 \
+  -pix_fmt yuv420p \
+  -acodec mp3 \
+  -t 10 \
+  output.mp4
