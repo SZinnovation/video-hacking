dd49c8a65ad2384bbb3a8331bb6943948f2b6b80
Dav Clark <dav@bead.glass>
2018-02-08 12:54:57 -0500

    Updated fujisawa

--- scripts/ffmpeg-fujisawa.sh
+++ scripts/ffmpeg-fujisawa.sh
@@ -1,11 +1,13 @@
 ffmpeg -hwaccel cuvid \
-  -f v4l2 -framerate 30 -video_size hd720 -pixel_format yuv420p \
+  -f v4l2 -framerate 30 -video_size hd720 -input_format yuv420p \
   -thread_queue_size 1024 \
   -i /dev/video1 \
   -f alsa -thread_queue_size 1024 \
   -i hw:4 \
   -threads 4 \
+  -acodec aac -strict -2 \
+  -vcodec libx264 \
   -pix_fmt yuv420p \
-  -acodec mp3 \
+  -preset veryfast \
   -t 10 \
   output.mp4
