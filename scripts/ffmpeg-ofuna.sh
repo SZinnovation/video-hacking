c235530ab618d31afdbe9a1c75878d054618d13e
Dav Clark <dav@bead.glass>
2018-04-11 01:08:36 +0900

    small modifications for better quality

--- scripts/ffmpeg-ofuna.sh
+++ scripts/ffmpeg-ofuna.sh
@@ -4,12 +4,20 @@
 # good for video encoding, but folks claim that CPU encoding with x264 yeilds
 # better quality, and is fast enough).
 
+# Note that I can't prevent dropped frames. And I get a lot of them. The video
+# seems OK, though. I tried `-r 30` in the codec and it forced framerate to 30
+# in the status for ffmpeg, but otherwise still had dropped frames.
+
 # Don't think I need this, but can supply right after hwaccel if needed
 # -vaapi_device /dev/dri/renderD128 \
 
 # Removed from acodec section (for output) - no longer needed for aac on more
 # recent versions of ffmpeg
 # -strict -2 \
+
+# I removed this to avoid the second pass at the end, so it doesn't take as
+# long for ffmpeg to finalize:
+# -movflags faststart \
 
 # This seems necessary but not sufficient for the XBox microphone, which has 4
 # channels. We should explore using two for one camera and two for the other!
@@ -27,8 +35,8 @@ ffmpeg -hwaccel vaapi \
   -threads 4 \
   -codec:a aac \
   -codec:v libx264 \
+  -preset faster \
+  -crf 20 \
   -pix_fmt yuv420p \
-  -preset veryfast \
-  -movflags faststart \
   -t 49:00 \
   ${dt}-ofuna-${desc}-video0-alsa1-hd1080.mp4
