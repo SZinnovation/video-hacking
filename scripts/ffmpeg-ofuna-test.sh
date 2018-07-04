7e9060440e4100df3fc7fb3eda1285104974bad5
szproto root <root@szproto>
2018-07-05 07:18:36 +0900

    Changing back to ALSA, ready for stereo

--- scripts/ffmpeg-ofuna-test.sh
+++ scripts/ffmpeg-ofuna-test.sh
@@ -4,19 +4,21 @@
 
 dt=$(date +%Y%m%dT%H%M)
 desc=${1:-unknown}
+video=${2:-video0}
+alsa=${3:-0}
 
 ffmpeg -hide_banner -nostats \
   -hwaccel vaapi \
   -f v4l2 -framerate 30 -video_size hd720 -input_format yuv420p \
   -thread_queue_size 1024 \
-  -i /dev/video0 \
-  -f pulse -thread_queue_size 1024 \
-  -i 1 \
+  -i /dev/$video \
+  -f alsa -thread_queue_size 1024 \
+  -i hw:$alsa \
   -threads 4 \
   -codec:a aac \
   -codec:v libx264 \
-  -preset faster \
+  -preset veryfast \
   -crf 20 \
   -pix_fmt yuv420p \
   -t 5 \
-  ${dt}-ofuna-${desc}-video0-alsa1-hd720.mp4
+  ${dt}-ofuna-${desc}-${video}-alsa${alsa}-hd720.mp4
